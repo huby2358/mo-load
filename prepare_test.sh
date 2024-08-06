f3317441229ac31895440fb55407043c2fc324ab
Jensen <jensenojs@qq.com>
2024-08-06 13:53:33 +0800

    fix time

--- prepare_test.sh
+++ prepare_test.sh
@@ -19,17 +19,18 @@ function execute_task(){
 
     echo "开始执行:./start.sh -c $file_path/$script_name -d $data -t $concurrent -b $base"
     start_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-    echo "开始时间: ${start_time} CST"
+    echo "开始时间: ${start_time} UTC"
 
     # 执行具体任务
     ./start.sh -c "$file_path/$script_name" -d $data -t $concurrent -b $base
     end_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-    echo 结束时间: ${end_time} CST""
+    echo 结束时间: ${end_time} UTC""
 }
 
 # 修正 for 循环语法
 for script in "${script_order[@]}"; do
     for concurrent in "${concurrent_array[@]}"; do
         execute_task "$script" 1 "$concurrent" "jensen"
+        # execute_task "$script" 5 "$concurrent" "jensen"
     done
 done
